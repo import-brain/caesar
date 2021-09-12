aa01189316a69729b3aff566e010a183ffa869ef
Eric Cheng <68196762+import-brain@users.noreply.github.com>
2021-09-12 16:30:14 -0400

    streamline conversion control flow
    more optimizations
    
    fixed iteration printf mistake
    
    streamline argc control flow, delete repetitive code
    
    .

--- caesar.c
+++ caesar.c
@@ -9,24 +9,30 @@ string caesar(string input, string output, int key);
 
 int main(int argc, string argv[])
 {
+
+    bool failure = false;
+
     if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
-    int argvToInt = atoi(argv[1]);
-
-    bool failure = false;
-
     for (int i = 0, n = strlen(argv[1]); i < n; i++)
     {
-        if (isalpha(argv[1][i]) || ispunct(argv[1][i]) || !isdigit(argv[1][i]) || isblank(argv[1][i] || isspace(argv[1][i])))
+        if (!isdigit(argv[1][i]))
         {
-            printf("Usage: ./caesar key\n");
-            return 1;
+            failure = true;
         }
     }
+
+    if (failure == true)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    int argvToInt = atoi(argv[1]);
 
     string plaintext = get_string("plaintext: ");
 
@@ -45,20 +51,16 @@ string caesar(string input, string output, int key)
         if (isupper(input[i]))
         {
             int asciiCurrent = (int) input[i];
-            int asciiConverted = ((((asciiCurrent - 97) + key) % 26) + 97);
+            int asciiConverted = (((asciiCurrent - 97 + key) % 26) + 97);
             output[i] = (char) asciiConverted;
         }
         else if (islower(input[i]))
         {
             int asciiCurrent = (int) input[i];
-            int asciiConverted = ((((asciiCurrent - 65) + key) % 26) + 65);
+            int asciiConverted = (((asciiCurrent - 65 + key) % 26) + 65);
             output[i] = (char) asciiConverted;
         }
-        else if (isspace(input[i]))
-        {
-            output[i] = input[i];
-        }
-        else if (ispunct(input[i]))
+        else if (isspace(input[i]) || ispunct(input[i]))
         {
             output[i] = input[i];
         }
